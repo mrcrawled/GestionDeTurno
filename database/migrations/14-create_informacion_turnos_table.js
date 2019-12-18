2f1219191608c2e44aef3056bfc07995e0df61bf
Mr Crawled <fertepsi@gmail.com>
2019-12-18 18:26:56 -0300

    Se realizarion cambios en el modelo relacional

---
+++ database/migrations/14-create_informacion_turnos_table.js
@@ -0,0 +1,16 @@
+exports.up = (knex) => {
+    return Promise.all ([   
+        knex.schema.createTable('informacion_turnos', table =>{
+        table.increments('id').primary();
+        table.integer('id_turno').references('turnos.id').onDelete('cascade').onUpdate('restrict').notNullable();
+        table.datetime('fecha_hora_turno').notNullable();
+        table.date('fecha_alta',{ useTz: true }).defaultTo(knex.fn.now());
+        table.date('fecha_baja',{ useTz: true }).defaultTo(knex.fn.now());
+        table.text('descripcion');
+    })
+]);
+};
+
+exports.down = (knex) => {
+    return Promise.all ([knex.schema.dropTable('informacion_turnos')]);
+};
