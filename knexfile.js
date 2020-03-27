a5aed621e98a962bc940d5f3d84ddca208a5ce85
Mr Crawled <fertepsi@gmail.com>
2020-03-27 13:07:29 -0300

    Heroku init

--- knexfile.js
+++ knexfile.js
@@ -12,22 +12,24 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'admin123',
-      database : 'gestionturno',
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'admin123',
+      database: 'gestionturno',
       charset: 'utf8'
     },
     migrations,
     seeds,
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    use_env_variable: "DATABASE_URL"
+
   },
 
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -40,18 +42,13 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: process.env.DATABASE_URL,
+      charset: 'utf8'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-};
+    migrations,
+    seeds,
+    useNullAsDefault: true,
+  },
+}
