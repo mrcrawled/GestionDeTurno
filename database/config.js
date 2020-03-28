28e050be53b20df4220faaadcdb3133279ac9401
Mr Crawled <fertepsi@gmail.com>
2020-03-28 16:54:39 -0300

    Fix

--- database/config.js
+++ database/config.js
@@ -1,12 +1,7 @@
 const { Pool } = require('pg')
 
-const connectionString = new Pool({
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    user:  process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database:process.env.PGDATABASE,
-    charset: process.env.PGCHARSET,
-  });
+const connectionString = new Pool(
+   "postgres://*USERNAME*:*PASSWORD*@*HOST*:*PORT*/*DATABASE*"
+);
 
 module.exports = connectionString;
