237c79f707ebdb7596e926ad6dccca68831b070f
Mr Crawled <fertepsi@gmail.com>
2020-03-29 16:14:12 -0300

    Se agrego la configuracion en knexfile para produccion

--- database/config.js
+++ database/config.js
@@ -1,12 +1,7 @@
 const { Pool } = require('pg')
 
 const connectionString = new Pool({
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    user:  process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database:process.env.PGDATABASE,
-    charset: process.env.PGCHARSET,
-  });
-
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 module.exports = connectionString;
