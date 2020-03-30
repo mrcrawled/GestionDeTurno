088ab751168da164eb5c561050f85df99ed9825b
Mr Crawled <fertepsi@gmail.com>
2020-03-30 10:55:18 -0300

    Se agrego la configuracion en knexfile para produccion

--- server.js
+++ server.js
@@ -34,13 +34,6 @@ router.use((err, req, res, next) => {
 
 app.use('/',router);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    const path = require('path');
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
-}
 
 app.listen(process.env.PORT || 8000, (err)=>{
     if(err){
