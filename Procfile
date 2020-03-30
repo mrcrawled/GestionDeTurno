66fc231982dca41c6ee22ee7028e8d7075d1b173
Mr Crawled <fertepsi@gmail.com>
2020-03-30 08:45:47 -0300

    Se agrego la configuracion en knexfile para produccion

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: knex migrate:latest && knex migrate:rollback && knex seed:run && node server.js
--- database/config.js
+++ database/config.js
@@ -1,7 +1,12 @@
 const { Pool } = require('pg')
 
 const connectionString = new Pool({
-  connectionString: process.env.DATABASE_URL,
-
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user:  process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database:process.env.PGDATABASE,
+  charset: process.env.PGCHARSET,
 });
+
 module.exports = connectionString;
